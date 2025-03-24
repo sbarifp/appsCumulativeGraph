736741f201a7cff97af5cd289f62b8f974cc42fb
sbarifp <setyabudiarifprabowo9f29@gmail.com>
2025-03-24 09:04:27 +0700

    Update MACC App

--- macc_app_v2.py
+++ macc_app_v2.py
@@ -21,9 +21,9 @@ if uploaded_file:
     st.subheader("🧩 Pilih Kolom untuk Setiap Komponen")
 
     col_project = st.selectbox("Kolom Nama Proyek", df.columns)
-    col_x = st.selectbox("Kolom Sumbu X (AF / Pengurangan Emisi)", df.columns)
-    col_middle = st.selectbox("Kolom Nilai Tengah Batang (NPV)", df.columns)
-    col_top = st.selectbox("Kolom Nilai Ujung Batang (MAC)", df.columns)
+    col_x = st.selectbox("Kolom Sumbu X", df.columns)
+    col_middle = st.selectbox("Kolom Nilai Tengah Batang", df.columns)
+    col_top = st.selectbox("Kolom Nilai Ujung Batang", df.columns)
 
     st.markdown("---")
     st.subheader("⚙️ Parameter Tambahan")
